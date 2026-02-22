e9dd011e3abe47669a148fe1e3f85a0b1d9b15b7
divya kumrawat <divyakumrawat2005@gmail.com>
2026-02-22 15:17:44 +0530

    new

---
+++ loops/q1.py
@@ -0,0 +1,6 @@
+numbers = [1,-2,3,-5,-7,6,-8,9,10]
+pnc = 0
+for num in numbers:
+    if num > 0:
+        pnc += 1
+print("final count of positive number is:",pnc)
---
+++ loops/q5.py
@@ -0,0 +1,8 @@
+input_str = "teeter"
+
+
+for char in input_str:
+    print(char)
+    if input_str.count(char) == 1:
+        print("char is: ",char)
+        break
